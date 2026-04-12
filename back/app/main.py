d8ae98e4bea4abda6b1c8eb1580c84f7c5bcd4fc
wooseok <ajpffehl87@gmail.com>
2026-04-12 16:15:31 +0900

    front vercel 배포

--- back/app/main.py
+++ back/app/main.py
@@ -13,7 +13,9 @@ from fastapi.middleware.cors import CORSMiddleware
 Base.metadata.create_all(bind=engine)
 
 origins = [
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "https://cluster-one-beta.vercel.app",
+    "https://*.vercel.app",
 ]
 
 def create_app():
